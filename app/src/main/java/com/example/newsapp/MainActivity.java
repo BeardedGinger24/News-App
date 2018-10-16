78fb5bffd48b203ab01d2274ed077aa83e8b125d
Mher Oganesyan <mher_95@hotmail.com>
2018-10-15 19:42:26 -0700

    Recycler View Complete, Listener

--- app/src/main/java/com/example/newsapp/MainActivity.java
+++ app/src/main/java/com/example/newsapp/MainActivity.java
@@ -1,11 +1,18 @@
 package com.example.newsapp;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.view.View.OnClickListener;
+import android.widget.Toast;
 
 import com.example.newsapp.model.NewsItem;
 import com.example.newsapp.utilities.NetworkUtils;
@@ -14,18 +21,29 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NewsAdapter.ListItemClickListener {
 
-    TextView mNewsJsonResults;
+    //TextView mNewsJsonResults;
+    NewsAdapter mNewsAdapter;
+    RecyclerView mNewsArticles;
+    ArrayList<NewsItem> articles;
+    //Toast mToast;
+    //Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mNewsJsonResults = (TextView) findViewById(R.id.tv_json_news_result);
+        //mNewsJsonResults = (TextView) findViewById(R.id.tv_json_news_result);
+        mNewsArticles = (RecyclerView) findViewById(R.id.rv_news_articles);
 
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        mNewsArticles.setLayoutManager(layoutManager);
+
+        mNewsArticles.setHasFixedSize(true);
         makeNewsSearchQuery();
+
     }
 
     private void makeNewsSearchQuery(){
@@ -55,9 +73,12 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(String newsSearchResults) {
             super.onPostExecute(newsSearchResults);
-            ArrayList<NewsItem> testList = NetworkUtils.parseJson(newsSearchResults);
-            String test = testList.get(0).getAuthor();
-            mNewsJsonResults.setText(test);
+            articles = NetworkUtils.parseJson(newsSearchResults);
+
+            mNewsAdapter = new NewsAdapter( articles, MainActivity.this);
+            mNewsArticles.setAdapter(mNewsAdapter);
+            //String test = articles.get(0).getAuthor();
+            //mNewsJsonResults.setText(test);
         }
     }
 
@@ -75,5 +96,18 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onListItemClick(int clickedItemIndex) {
+//        mToast = Toast.makeText(this, "Hurray", Toast.LENGTH_LONG);
+//        mToast.show();
+
+        Uri webpage = Uri.parse(articles.get(clickedItemIndex).getUrl());
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+
+        if(intent.resolveActivity(getPackageManager()) != null){
+            startActivity(intent);
+        }
     }
 }
--- app/src/main/java/com/example/newsapp/NewsAdapter.java
+++ app/src/main/java/com/example/newsapp/NewsAdapter.java
@@ -1,5 +1,84 @@
 package com.example.newsapp;
 
-public class NewsAdapter {
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.example.newsapp.model.NewsItem;
+
+import java.util.ArrayList;
+
+public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ArticleViewHolder> {
+
+    //Context context;
+    private ArrayList<NewsItem> articles;
+    final private ListItemClickListener mOnClickListener;
+
+
+    public NewsAdapter(ArrayList<NewsItem> articles, ListItemClickListener mOnClickListener){
+        //this.context = context;
+        this.articles = articles;
+        this.mOnClickListener = mOnClickListener;
+    }
+
+    public interface ListItemClickListener {
+        void onListItemClick(int clickedItemIndex);
+    }
+
+    @Override
+    public ArticleViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        Context context = viewGroup.getContext();
+        int layoutIdForListItem = R.layout.list_item;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        boolean shouldAttachToParentImmediately = false;
+
+        View view = inflater.inflate(layoutIdForListItem, viewGroup, shouldAttachToParentImmediately);
+        ArticleViewHolder viewHolder = new ArticleViewHolder(view);
+
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder( ArticleViewHolder articleViewHolder, int i) {
+        articleViewHolder.bind(i);
+    }
+
+    @Override
+    public int getItemCount() {
+        return articles.size();
+    }
+
+    class ArticleViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+
+
+        TextView mArticleTitle;
+        TextView mArticleDescription;
+        TextView mArticleDate;
+
+
+        public ArticleViewHolder(View itemView) {
+            super(itemView);
+
+            mArticleTitle = (TextView) itemView.findViewById(R.id.tv_article_title);
+            mArticleDescription = (TextView) itemView.findViewById(R.id.tv_article_description);
+            mArticleDate = (TextView) itemView.findViewById(R.id.tv_article_date);
+
+            itemView.setOnClickListener(this);
+        }
+
+         void bind(int position){
+            mArticleTitle.setText(articles.get(position).getTitle());
+            mArticleDescription.setText(articles.get(position).getDescription());
+            mArticleDate.setText(articles.get(position).getPublishedAt());
+         }
+
+        @Override
+        public void onClick(View v) {
+            int clickedPosition = getAdapterPosition();
+            mOnClickListener.onListItemClick(clickedPosition);
+        }
+    }
 }
